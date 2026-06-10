a6108a6b6ed598f5acd8247ea44f61e9329712a1
Iskandar Nuhu <iskandar172nuhu@gmail.com>
2026-06-10 19:11:10 +0100

    Update frontend API base URL for ECS ALB

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5050/api",
+  baseURL: "http://opsflow-alb-489271614.eu-west-2.elb.amazonaws.com/api",
 });
 
 api.interceptors.request.use((config) => {
